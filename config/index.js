623923497d555bb7eb58e201bf2117085e970eae
hanyi <hanyitim@yeah.net>
2020-03-27 15:01:44 +0800

    fix:修复bug

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
-// const HOST = '//node.guaiyu.online';
-const HOST = '//192.168.10.17:8090';
+const HOST = '//node.guaiyu.online';
+// const HOST = '//192.168.10.17:8090';
 
 module.exports = {
     HOST
