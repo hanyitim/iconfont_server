b07e6c0f7fdac2869572f1643635a00ab5b5b4f3
hanyi <hanyitim@yeah.net>
2020-06-03 13:35:30 +0800

    fix:修复bug

--- gulpTask/includeLess.js
+++ gulpTask/includeLess.js
@@ -3,21 +3,21 @@ const unit = require('./_unit.js');
 const inject = require('gulp-inject-string');
 const fs = require('fs');
 module.exports = function(fontName,fileName){
-    var lessRules = [];
-    var fileContent = "",
-        content = {},
-        selectIcon = [];
-    try{
-        fileContent = fs.readFileSync(fileName ? unit.pathInterim(`${fileName}.json`) :unit.pathPublic('config',fontName,'config.json'),{encoding:'utf-8'});
-        content = JSON.parse(fileContent);
-        selectIcon = content.list;
-    }catch(e){
-        console.log(e);
-    }
-    Array.isArray(selectIcon) && selectIcon.forEach((item)=>{
-        lessRules.push(`.${item.className}{.icon("${item.content}")}`);
-    });
     return function(){
+        var lessRules = [];
+        var fileContent = "",
+            content = {},
+            selectIcon = [];
+        try{
+            fileContent = fs.readFileSync(fileName ? unit.pathInterim(`${fileName}.json`) :unit.pathPublic('config',fontName,'config.json'),{encoding:'utf-8'});
+            content = JSON.parse(fileContent);
+            selectIcon = content.list;
+        }catch(e){
+            console.log(e);
+        }
+        Array.isArray(selectIcon) && selectIcon.forEach((item)=>{
+            lessRules.push(`.${item.className}{.icon("${item.content}")}`);
+        });
         return src(unit.pathUnzip(fontName,'/index.less'))
         .pipe(inject.append(lessRules.join('\n')))
         .pipe(dest(unit.pathUnzip(fontName)))
